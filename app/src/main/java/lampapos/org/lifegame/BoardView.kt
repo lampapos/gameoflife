69e4b863bb727ac355cade020fbec034af32d6f7
Michael Pustovit <pustovitm@gmail.com>
2018-10-10 12:13:16 +0300

    Remove tools tags from layouts

--- app/src/main/java/lampapos/org/lifegame/BoardView.kt
+++ app/src/main/java/lampapos/org/lifegame/BoardView.kt
@@ -1,10 +1,8 @@
 package lampapos.org.lifegame
 
-import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.util.AttributeSet
 import android.view.View
 
 class BoardView : View {
